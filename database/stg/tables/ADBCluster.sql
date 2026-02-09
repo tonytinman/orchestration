21a3d86e822d82a81bcdb6aa766d8839244ac580
tonywilkinson <ajwilkinson1@hotmail.com>
2026-02-09 21:03:52 +0000

    Create ADBCluster.sql

---
+++ database/stg/tables/ADBCluster.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [stg].[ADBCluster] (
+    [ProcessGroupName]      NVARCHAR (255) NOT NULL,
+    [dev_cluster_id]    NVARCHAR (255) NOT NULL,
+    [ccd_cluster_id]    NVARCHAR (255) NOT NULL,
+    [pre_cluster_id]    NVARCHAR (255) NOT NULL,
+    [prd_cluster_id]    NVARCHAR (255) NOT NULL
+);
+GO
